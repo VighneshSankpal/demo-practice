df2818182555460ff1776a942012b6c8e1bb1811
Vighnesh <vighneshsankpal7@gmail.com>
2025-11-04 11:33:16 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+from data import quetions,options,ans
+
+
+print("Welcome to the KBC game.")
+print("You get 10 questions and 4 options you choose the correct options and the game will continue untill you answer wrong.")
